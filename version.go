c62c8594339639b63c92c43b86b911a8cb69209b
Steef de Rooi <steef@Bakvis.fritz.box>
2018-07-26 20:48:51 +0200

    updated comment

--- version.go
+++ version.go
@@ -1,3 +1,5 @@
+// This file will be overwritten by the IBFD CI system on deployment
+// The version number will be replaced by the one defined in ConMan.
 package main
 
-const version = "123";
+const version = "1.0"
