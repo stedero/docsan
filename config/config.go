b59ab89652889ebcb9e41344e3105aba65ce2d88
Steef de Rooi <s.derooi@ibfd.org>
2018-07-30 11:51:51 +0200

    read configuration from config file

--- config/config.go
+++ config/config.go
@@ -1,13 +1,41 @@
 package config
 
 import (
+	"encoding/json"
 	"flag"
+	"io/ioutil"
+	"log"
 	"os"
 )
 
 // DefaultPort defines the port to use if not defined
 // by the environment or on the command line
 const DefaultPort = "8080"
+const configFilename = "config.json"
+
+// Config defines the structure of the config.json file
+type Config struct {
+	MetaTags []string `json:"meta_tags"`
+}
+
+var allowedMetaNames map[string]bool
+
+func init() {
+	data, err := ioutil.ReadFile(configFilename)
+	if err != nil {
+		log.Fatalf("fail to read file %s: %v", configFilename, err)
+	}
+	var config Config
+	err = json.Unmarshal(data, &config)
+	if err != nil {
+		log.Fatalf("fail to unmarshal from file %s: %v", configFilename, err)
+	}
+	allowedMetaNames = make(map[string]bool, len(config.MetaTags))
+	for _, metaName := range config.MetaTags {
+		allowedMetaNames[metaName] = true
+	}
+	log.Printf("loaded configuration from %s", configFilename)
+}
 
 // GetPort returns the port to use for the Docsan service
 func GetPort() string {
@@ -25,17 +53,8 @@ func GetPort() string {
 // MetaNameAccept returns a function to filter meta tags
 // TODO: Read allowed meta tags from config file.
 func MetaNameAccept() func(string) bool {
-	allowedMetaNames := map[string]bool{
-		"authorize_file":   true,
-		"collection":       true,
-		"pdf_chapter":      true,
-		"print_version":    true,
-		"titleframe_title": true,
-		"word_chapter":     true}
-
 	return func(metaName string) bool {
 		_, present := allowedMetaNames[metaName]
 		return present
 	}
-
 }
