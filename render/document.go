4f00d3048dc51608b2be869103d9f9cb269f5866
Steef de Rooi <s.derooi@ibfd.org>
2018-12-17 16:51:50 +0100

    #9540: Implement International Tax Structuring summarizing table

--- render/document.go
+++ render/document.go
@@ -19,6 +19,7 @@ type Document struct {
 	Generated string              `json:"generated"`
 	Title     string              `json:"title"`
 	Outline   *JSON               `json:"outline"`
+	Sumtab    *JSON               `json:"sumtab"`
 	Metas     []map[string]string `json:"metas"`
 	Links     []map[string]string `json:"links"`
 	Scripts   []map[string]string `json:"scripts"`
@@ -29,17 +30,19 @@ type Document struct {
 func Transform(htmlDoc *html.Node, generated string) *Document {
 	scriptSelector := node.Element("script")
 	outLineAttrChecker := node.AttrEquals("id", "outline")
+	sumtabAttrChecker := node.AttrEquals("id", "sumtab")
 	head := node.FindFirst(htmlDoc, node.Element("head"))
 	title := node.FindFirst(head, node.Element("title"))
-	jsonOutline := formatOutline(node.FindFirst(htmlDoc, node.And(scriptSelector, outLineAttrChecker)))
+	jsonOutline := formatJSON(node.FindFirst(htmlDoc, node.And(scriptSelector, outLineAttrChecker)))
+	jsonSumtab := formatJSON(node.FindFirst(htmlDoc, node.And(scriptSelector, sumtabAttrChecker)))
 	metas := node.FindAll(head, node.Element("meta"))
 	links := node.FindAll(head, node.Element("link"))
-	scripts := node.FindAll(head, node.And(scriptSelector, node.Not(outLineAttrChecker)))
+	scripts := node.FindAll(head, node.And(scriptSelector, node.Not(node.Or(outLineAttrChecker, sumtabAttrChecker))))
 	body := node.FindFirst(htmlDoc, node.Element("body"))
 	san1Body := node.AddActionBarDivs(body, actionBarTargetSelector())
 	san2Body := node.ReplaceWithComments(san1Body, commentTargetSelector())
 	san3Body := node.DisableAttribute(san2Body, "onclick")
-	return newDocument(generated, title, jsonOutline, metas, links, scripts, san3Body)
+	return newDocument(generated, title, jsonOutline, jsonSumtab, metas, links, scripts, san3Body)
 }
 
 // ToJSON renders a document to JSON.
@@ -50,11 +53,12 @@ func (document *Document) ToJSON(w io.Writer) error {
 }
 
 // newDocument create a new document
-func newDocument(generated string, title *html.Node, jsonOutline *JSON, metas []*html.Node, links []*html.Node, scripts []*html.Node, sanitizedBody *html.Node) *Document {
+func newDocument(generated string, title *html.Node, jsonOutline *JSON, jsonSumtab *JSON, metas []*html.Node, links []*html.Node, scripts []*html.Node, sanitizedBody *html.Node) *Document {
 	return &Document{
 		Generated: "docsan " + generated,
 		Title:     node.Content(title),
 		Outline:   jsonOutline,
+		Sumtab:    jsonSumtab,
 		Metas:     toMetas(metas),
 		Links:     node.ToMapArray(links),
 		Scripts:   node.ToMapArray(scripts),
@@ -98,7 +102,7 @@ func metaAccept(acceptMetaName func(string) bool) node.CheckAttrs {
 	}
 }
 
-func formatOutline(n *html.Node) *JSON {
+func formatJSON(n *html.Node) *JSON {
 	var data string
 	if n == nil {
 		data = "{}"
