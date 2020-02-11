90a85476a0fcec6755360c3394d620cf0e511ded
Steef de Rooi <s.derooi@ibfd.org>
2020-02-11 13:36:51 +0100

    added glossary entries as JSON

--- render/document.go
+++ render/document.go
@@ -3,6 +3,8 @@ package render
 import (
 	"encoding/json"
 	"io"
+	"strings"
+	"regexp"
 
 	"golang.org/x/net/html"
 	"ibfd.org/docsan/config"
@@ -32,7 +34,20 @@ type Document struct {
 	Tables    *JSON               `json:"tables"`
 	Metas     []map[string]string `json:"metas"`
 	Scripts   []map[string]string `json:"scripts"`
+	Glossary  []*GlossaryEntry    `json:"glossary"`
 	Body      string              `json:"body"`
+}
+
+// GlossaryEntry defines a glossary article.
+type GlossaryEntry struct {
+	Title string `json:"title"`
+	ID    string `json:"id"`
+}
+
+var spaceRE *regexp.Regexp
+
+func init() {
+	spaceRE = regexp.MustCompile(`\s+`)
 }
 
 // Transform transforms a HTML node to a document structure for JSON output.
@@ -52,6 +67,7 @@ func Transform(htmlDoc *html.Node, generated string) *Document {
 	jsonRefs := formatJSON(node.FindFirst(htmlDoc, node.And(scriptSelector, refsAttrChecker)), jsonObject)
 	jsonTables := formatJSON(node.FindFirst(htmlDoc, node.And(scriptSelector, tablesAttrChecker)), jsonArray)
 	metas := node.FindAll(head, node.Element("meta"))
+	glossary := node.FindFirst(htmlDoc, glossarySelector())
 	scriptsToDelete := node.And(scriptSelector, node.Or(outLineAttrChecker, sumtabAttrChecker, linksAttrChecker, refsAttrChecker, tablesAttrChecker, tocAttrChecker))
 	scripts := node.FindAll(head, node.And(scriptSelector, node.Not(scriptsToDelete)))
 	body := node.FindFirst(htmlDoc, node.Element("body"))
@@ -61,7 +77,7 @@ func Transform(htmlDoc *html.Node, generated string) *Document {
 	san4Body := node.ReplaceWithComments(san3Body, commentTargetSelector())
 	san5Body := node.WrapTables(san4Body, chapterTableSelector())
 	san6Body := node.DisableAttribute(san5Body, "onclick", disableAtributeSelector())
-	return newDocument(generated, title, jsonOutline, jsonSumtab, jsonLinks, jsonRefs, jsonTables, metas, scripts, san6Body)
+	return newDocument(generated, title, jsonOutline, jsonSumtab, jsonLinks, jsonRefs, jsonTables, metas, glossary, scripts, san6Body)
 }
 
 // ToJSON renders a document to JSON.
@@ -76,7 +92,7 @@ func (document *Document) ToJSON(w io.Writer) error {
 }
 
 // newDocument create a new document
-func newDocument(generated string, title *html.Node, jsonOutline *JSON, jsonSumtab *JSON, jsonLinks *JSON, jsonRefs *JSON, jsonTables *JSON, metas []*html.Node, scripts []*html.Node, sanitizedBody *html.Node) *Document {
+func newDocument(generated string, title *html.Node, jsonOutline *JSON, jsonSumtab *JSON, jsonLinks *JSON, jsonRefs *JSON, jsonTables *JSON, metas []*html.Node, glossary *html.Node, scripts []*html.Node, sanitizedBody *html.Node) *Document {
 	return &Document{
 		Generated: "docsan " + generated,
 		Title:     node.Content(title),
@@ -86,6 +102,7 @@ func newDocument(generated string, title *html.Node, jsonOutline *JSON, jsonSumt
 		SeeAlso:   jsonRefs,
 		Tables:    jsonTables,
 		Metas:     toMetas(metas),
+		Glossary:   extractGlossaryArticles(glossary),
 		Scripts:   node.ToMapArray(scripts),
 		Body:      node.RenderChildrenCommentParent(sanitizedBody)}
 }
@@ -165,6 +182,26 @@ func metaAccept(acceptMetaName func(string) bool) node.CheckAttrs {
 		name, present := attrs["name"]
 		return !present || acceptMetaName(name)
 	}
+}
+
+func glossarySelector() node.Check {
+	isMain := node.Element("main")
+	isGlossaryType := node.AttrContains("class", "glossary")
+	return node.And(isMain, isGlossaryType)
+}
+
+func extractGlossaryArticles(glossary *html.Node) []*GlossaryEntry {
+	articles := node.FindAll(glossary, node.Element("article"))
+	entries := make([]*GlossaryEntry, 0, len(articles))
+	for _, article := range articles {
+		attrs := node.AttrsAsMap(article)
+		id := attrs["id"]
+		hdr := node.FindFirst(article, node.Element("h3"))
+		title := strings.TrimSpace(spaceRE.ReplaceAllString(node.Content(hdr), " "))
+		ge := &GlossaryEntry{title, id}
+		entries = append(entries, ge)
+	}
+	return entries
 }
 
 func formatJSON(n *html.Node, jtype jsonType) *JSON {
